0e7717c40d2d8ecc06d66f3e0c7c9c83c0a5e1bf
Joniselury Rasco <joniseluryrasco@gmail.com>
2024-03-21 16:46:02 +0800

    Add project files.

---
+++ ePortal.WebAPI/DTO/bible_versesDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ePortal.WebAPI.DTO
+{    public class BibleVerseDTO
+    {
+        public string Verse { get; set; }
+        public string Chapter { get; set; }
+        public DateOnly? Date { get; set; }
+        public bool? Archive { get; set; }
+    }
+
+    public class BibleVerseLatestDTO
+    {
+        public string Verse { get; set; }
+        public string Chapter { get; set; }
+        public DateOnly? Date { get; set; }
+    }
+}
---
+++ ePortal.WebAPI/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//SQL Connection
+builder.Services.AddDbContext<ePortal.WebAPI.Models.pgas_eportal_v2Context>(
+    options =>
+    { 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("PGAS_ePortal_v2"));    
+    }
+    );
+
+var app = builder.Build();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin  
+    .AllowCredentials());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
